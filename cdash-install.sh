8e7e9e7b76aff0de1e0a2a9158c9626a1d2133c0
Lars Baehren <lbaehren@gmail.com>
2017-08-27 16:44:54 +0200

    Script for installation of CDash

---
+++ cdash-install.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# -----------------------------------------------------------------------------
+#  Variables
+
+# Installation location of CDash (as part of the webserver directory)
+cdash_basedir=/var/www/CDash
+
+# -----------------------------------------------------------------------------
+
+echo "-- Installing development tools ..."
+
+apt-get install -y nedit cmake git gcc g++ net-tools
+
+echo "-- Install server components ..."
+
+apt-get install -y apache2 mysql-server
+
+echo "-- Install PHP and extensions ..."
+
+apt-get install -y php php-mysql php-xsl php-curl php-gd php-dev php-xmlrpc php-bcmath php-mbstring php-xdebug
+
+echo "-- Restart MySQL database ..."
+
+rcmysql start
+
+# -----------------------------------------------------------------------------
+
+echo "-- Update the base system before any new installs ..."
+
+apt-get update --fix-missing
+
+# -----------------------------------------------------------------------------
+
+echo "-- Pull CDash sources from Git repository ..."
+
+cd /var/www
+git clone https://github.com/Kitware/CDash.git CDash
+cd ${cdash_basedir}
+git checkout prebuilt
+
+cd ${cdash_basedir}
+mkdir build
+cd build
+cmake ..
+
+cd ${cdash_basedir}/config
+cp config.php config.local.php
+nano config.local.php
+
+cd ${cdash_basedir}
+ln -s ${cdash_basedir}/public /var/www/html/CDash
+chmod a+rw log backup public/upload public/rss
+
+# -----------------------------------------------------------------------------
+#
+# Note: Typically the database entry needs to be done by hand; however using
+#       the '-e <command>' command line option this should be scriptable as
+#       well.
+#
+
+echo "-- Create MySQL database for CDash ..."
+echo ""
+echo "mysql> create database cdash;"
+echo "mysql> create user 'cdash'@'localhost' identified by '<password>';"
+echo "mysql> grant all privileges on cdash.* to 'cdash'@'localhost' with grant option;"
+echo ""
+
+mysql -u root -p
+
+# mysql> create database cdash;
+# mysql> create user 'cdash'@'localhost' identified by '${mysql_pass}';
+# mysql> grant all privileges on cdash.* to 'cdash'@'localhost' with grant option;
+
+# -----------------------------------------------------------------------------
+#  Configuration of apache
+
+echo "-- Configuration of CDash module for Apache ..."
+
+mkdir -p /etc/apache2/conf.d
+
+cat << 'EOF' >> /etc/apache2/conf.d/cdash.conf
+<Directory /srv/www/htdocs/CDash>
+   Order allow,deny
+   Allow from all
+</Directory>
+EOF
+
+echo "-- Restarting Apache server ..."
+
+service apache2 restart
