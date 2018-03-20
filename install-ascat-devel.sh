788f94063f7f2f36b26946be853e66c91de978b6
Lars Baehren <lbaehren@gmail.com>
2018-03-20 18:32:25 +0100

    Start generalizing ASCAT devel install script to work on multiple platforms.

--- install-ascat-devel.sh
+++ install-ascat-devel.sh
@@ -19,7 +19,6 @@
 ##
 ## =======================================================================================
 
-osVersion=6.6
 cmd_curl="curl -k -L"
 cmd_tar="tar -xzf"
 
@@ -28,58 +27,162 @@ METTOOLS_PREFIX=/opt/mettools
 
 ## =======================================================================================
 ##
+##  System inspection
+##
+## =======================================================================================
+
+osName=""
+osCPE_NAME=""
+osVersion=""
+
+if [[ -f /etc/os-release ]] ; then
+    osName=`grep NAME /etc/os-release | grep -v _NAME | tr "=" "\n" | grep [a-z]`
+    osCPE_NAME=`grep CPE_NAME /etc/os-release | tr '"' '\n' | grep cpe`
+    osVersion=`echo ${osCPE_NAME} | tr ":" "\n" | grep [0-9]`
+else
+    if [[ -f /etc/system-release-cpe ]] ; then
+        osCPE_NAME=`cat /etc/system-release-cpe`
+    fi
+    if [[ -f /etc/system-release ]] ; then
+        osName=`cat /etc/system-release | tr " " "\n" | head -n 1`
+        osVersion=`cat /etc/system-release | tr " " "\n" | grep [0-9]`
+    fi
+fi
+
+echo "------------------------------------------------------------"
+echo " Configuration summary"
+echo ""
+echo " - OS name      : ${osName}"
+echo " - OS CPE_NAME  : ${osCPE_NAME}"
+echo " - OS version   : ${osVersion}"
+echo " - Curl command : '${cmd_curl}'"
+echo " - Tar command  : '${cmd_tar}'"
+echo "------------------------------------------------------------"
+
+## =======================================================================================
+##
 ##  Package installation using package manager
 ##
 ## =======================================================================================
 
-echo "[install.sh] Installing system packages ..."
+#-----------------------------------------------------------------------------------------
+#  CentOS
+#-----------------------------------------------------------------------------------------
 
-yum install -y deltarpm nano net-tools yum-skip-broken
-yum groupinstall -y --skip-broken "Development tools"
-# --- specific development packages ---
-yum install -y bzip2
-yum install -y compat-libf2c-34.i686
-yum install -y curl
-yum install -y doxygen
-yum install -y expat-devel expat-devel.i686
-yum install -y fftw-devel fftw-devel.i686
-yum install -y gcc gcc-c++ gcc-fortran
-yum install -y git
-yum install -y glibc-devel glibc-devel.i686
-yum install -y graphviz
-yum install -y grep
-yum install -y kexec-tools
-yum install -y ksh
-yum install -y libaio
-yum install -y libdb-4_8-devel
-yum install -y libgfortran
-yum install -y libstdc++.i686
-yum install -y libX11-devel
-yum install -y libxml2 libxml2-devel
-yum install -y ncurses-devel
-yum install -y openssh openssl
-yum install -y patch
-yum install -y sed
-yum install -y sqlite-devel
-yum install -y tar
-yum install -y texinfo
-yum install -y valgrind valgrind.i686
-yum install -y which
-yum install -y xerces-c.i686 xerces-c-devel.i686
-yum install -y zlib zlib.i686
+install_packages_centos ()
+{
+    yum install -y deltarpm nano net-tools yum-skip-broken
+    yum groupinstall -y --skip-broken "Development tools"
+    # --- specific development packages ---
+    yum install -y bzip2
+    yum install -y compat-libf2c-34.i686
+    yum install -y curl
+    yum install -y doxygen
+    yum install -y expat-devel expat-devel.i686
+    yum install -y fftw-devel fftw-devel.i686
+    yum install -y gcc gcc-c++ gcc-fortran
+    yum install -y git
+    yum install -y glibc-devel glibc-devel.i686
+    yum install -y graphviz
+    yum install -y grep
+    yum install -y kexec-tools
+    yum install -y ksh
+    yum install -y libaio
+    yum install -y libdb-4_8-devel
+    yum install -y libgfortran
+    yum install -y libstdc++.i686
+    yum install -y libX11-devel
+    yum install -y libxml2 libxml2-devel
+    yum install -y ncurses-devel
+    yum install -y openssh openssl
+    yum install -y patch
+    yum install -y sed
+    yum install -y sqlite-devel
+    yum install -y tar
+    yum install -y texinfo
+    yum install -y valgrind valgrind.i686
+    yum install -y which
+    yum install -y xerces-c.i686 xerces-c-devel.i686
+    yum install -y zlib zlib.i686
 
-case ${osVersion} in
-    "7.0.1406"|"7.1.1503"|"7.2.1511"|"7.3.1611"|"7.4.1708")
-        yum install -y hostname
-        yum install -y libstdc++-static.i686
-        ;;
-esac
+    case ${osVersion} in
+        "7")
+            yum install -y hostname
+            yum install -y libstdc++-static.i686
+            ;;
+    esac
 
-echo "-- Create missing symbolic links ..."
-ln -sf /usr/lib/libg2c.so.0 /usr/lib/libg2c.so
-echo "-- Create missing symbolic links ... done"
+    echo "-- Create missing symbolic links ..."
+    ln -sf /usr/lib/libg2c.so.0 /usr/lib/libg2c.so
+    echo "-- Create missing symbolic links ... done"
+}
 
-echo "[install.sh] Installing system packages ... done"
+#-----------------------------------------------------------------------------------------
+#  Fedora
+#-----------------------------------------------------------------------------------------
+
+install_packages_fedora ()
+{
+    echo "Nothing to do yet!"
+}
+
+#-----------------------------------------------------------------------------------------
+#  openSUSE
+#-----------------------------------------------------------------------------------------
+
+install_packages_opensuse ()
+{
+    zypper -n update
+    zypper install deltarpm nano net-tools
+    zypper install -t pattern devel_C_C++
+    # --- specific development packages ---
+    for NAME in  \
+        bzip2  \
+        compat-32bit  \
+        curl  \
+        doxygen  \
+        libexpat-devel  \
+        libexpat-devel-32bit  \
+        fftw3-devel  \
+        libfftw3-3-32bit \
+        f2c-32bit  \
+        gcc  \
+        gcc-c++  \
+        gcc-fortran  \
+        git  \
+        glibc-devel  \
+        glibc-devel-32bit \
+        graphviz  \
+        grep  \
+        kexec-tools  \
+        ksh  \
+        libaio  \
+        libdb-4_8-devel  \
+        libgfortran3  \
+        libgfortran3-32bit  \
+        libstdc++-devel  \
+        libstdc++-devel-32bit  \
+        libX11-devel  \
+        libxml2  \
+        libxml2-devel  \
+        ncurses-devel  \
+        openssh  \
+        openssl  \
+        patch  \
+        sed  \
+        sqlite-devel  \
+        tar  \
+        texinfo  \
+        valgrind  \
+        which  \
+        libxerces-c-devel  \
+        libxerces-c-3_1-32bit  \
+        zlib-devel  \
+        zlib-devel-32bit
+    {
+        zypper install ${NAME}
+    }
+}
 
 ## =======================================================================================
 ##
@@ -180,9 +283,33 @@ install_gsl ()
 
 ## =======================================================================================
 ##
-##  Package installation from source packages
+##  Script main
 ##
 ## =======================================================================================
+
+#-----------------------------------------------------------------------------------------
+#  Installation of system packages
+#-----------------------------------------------------------------------------------------
+
+echo "[install.sh] Installing system packages ..."
+
+case ${osName} in
+    "CentOS Linux"|"CentOS")
+        install_packages_centos
+        ;;
+    "Fedora")
+        install_packages_fedora
+        ;;
+    "openSUSE")
+        install_packages_opensuse
+        ;;
+esac
+
+echo "[install.sh] Installing system packages ... done"
+
+#-----------------------------------------------------------------------------------------
+#  Custom installation of additional packages
+#-----------------------------------------------------------------------------------------
 
 install_cmake
 install_boost
